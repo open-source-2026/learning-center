62b854de89bc373f8769cf5abaaaf86268519637
Yamil Tejada <yamil.tejada9.pumacayo10@gmail.com>
2026-09-22 11:29:32 -0500

    feat(about): add About page with translation support and lazy loading route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { Home } from './shared/presentation/views/home/home';
 
 const baseTitle = 'ACME Learning Center';
+const about = () =>
+  import('./shared/presentation/views/about/about').then(m => m.About);
+
 
 export const routes: Routes = [
 
   { path: 'home', component: Home, title: `${baseTitle} - Home` },
 
 
+  // Se aplica cuando el componente es cargado de manera perezosa (lazy loading)
+  { path: 'about', loadComponent: about, title: `${baseTitle} - About` }
 ];
